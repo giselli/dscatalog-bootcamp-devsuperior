236afde6de6573bed4fd44edfb841610a3f94bb3
Giselli Hara <gi@hara.ninja>
2021-05-21 14:39:29 -0300

    Database seeding

--- backend/src/main/java/dev/giselli/dscatalog/services/CategoryService.java
+++ backend/src/main/java/dev/giselli/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.giselli.dscatalog.entities.Category;
 import dev.giselli.dscatalog.repositories.CategoryRepository;
@@ -21,6 +22,9 @@ public class CategoryService {
 	// o Spring injetará uma dependência válida do CategoryRepository
 	private CategoryRepository repository;
 
+	@Transactional(readOnly = true) //readOnly não trava o banco de dados
+	//importar do Spring
+	//vários métodos relacionados a uma transação
 	public List<Category> findAll() {
 		// deve acessar o repository e chamar as categorias no banco de dados
 		return repository.findAll();
