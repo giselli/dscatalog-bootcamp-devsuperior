0f8f1eb664529346f7cd74dfc8f39950c3f95339
Giselli Hara <gi@hara.ninja>
2021-06-14 19:01:43 -0300

    Teste de integração para findAllPaged

--- backend/src/test/java/dev/giselli/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/dev/giselli/dscatalog/services/ProductServiceIT.java
@@ -5,45 +5,96 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import dev.giselli.dscatalog.dto.ProductDTO;
 import dev.giselli.dscatalog.repositories.ProductRepository;
 import dev.giselli.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional
+//Transactional faz com que mesmo que o primeiro teste delete um item, um teste seguinte confirme o total corretamente
 public class ProductServiceIT {
-	
+
 	@Autowired
 	private ProductService service;
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private Long existingId;
 	private Long nonExistingId;
 	private Long countTotalProducts;
 
 	@BeforeEach
-	void setUp() throws Exception{
+	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
 	}
-	
+
 	@Test
 	public void deleteShouldDeleteResourceWhenIdExists() {
-	
+
 		service.delete(existingId);
-		
+
 		Assertions.assertEquals(countTotalProducts - 1, repository.count());
-		//total de registros no banco é igual ao total de registros anterior - 1
-		
+		// total de registros no banco é igual ao total de registros anterior - 1
+
 	}
-	
+
 	@Test
 	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
-	
+
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			service.delete(nonExistingId);
 		});
 	}
+
+	@Test
+	public void findAllPageShouldReturnPageWhenPage0Size10() {
+
+		PageRequest pageRequest = PageRequest.of(0, 10);
+
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertFalse(result.isEmpty());
+		// resulta em falso se a página estuver vazia, sem objeto
+		Assertions.assertEquals(0, result.getNumber());
+		// se o número da página de resultado é realmente zero
+		Assertions.assertEquals(10, result.getSize());
+		// se o tamanho da página é realmente 10
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+		// se o total de produtos buscados é igual ao countTotalProducts
+	}
+
+	@Test
+	public void findAllPageShouldReturnEmptyPageWhenPageDoesNotExist() {
+
+		PageRequest pageRequest = PageRequest.of(50, 10);
+
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertTrue(result.isEmpty());
+		//deve ser verdadeiro que o resultado é vazio, pois o 50 não existe neste caso
+	}
+	
+	@Test
+	public void findAllPageShouldReturnSortedPageWhenSortByName() {
+
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		//testa se o nome do primeiro elemento é Macbook Pro
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+		
+	}
+
 }
